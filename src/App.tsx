57945cfec4f9302963b609f016f678a3fbbdaebd
Igor40ivskyi <sorokivskyi777@gmail.com>
2023-11-18 15:23:30 +0200

    prettier test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import {PageHeader} from "./layouts/PageHeader.tsx";
-import {CategoryPills} from "./components/CategoryPills.tsx";
-import {categories} from "./data/home.ts";
-import {useState} from "react";
+import { PageHeader } from "./layouts/PageHeader.tsx";
+import { CategoryPills } from "./components/CategoryPills.tsx";
+import { categories } from "./data/home.ts";
+import { useState } from "react";
 export default function App() {
+  const [selectedCategory, setSelectedCategoty] = useState(categories[0]);
 
-    const [selectedCategory, setSelectedCategoty] = useState(categories[0]);
-
-    return <div className="max-h-screen flex flex-col">
-        <PageHeader/>
-        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-
-            <div>Sidebar</div>
-            <div className="sticky top-0 bg-white z-10 pb-4">
-                <CategoryPills categories={categories}
-                selectedCategory={selectedCategory} onSelect={setSelectedCategoty}
-                />
-            </div>
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <CategoryPills
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onSelect={setSelectedCategoty}
+          />
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
-
