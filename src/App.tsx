5c909c2abed04ecc024d009863a31fec9377256f
Igor40ivskyi <sorokivskyi777@gmail.com>
2023-11-18 21:03:45 +0200

    done the category pills slider

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { CategoryPills } from "./components/CategoryPills.tsx";
 import { categories } from "./data/home.ts";
 import { useState } from "react";
 export default function App() {
-  const [selectedCategory, setSelectedCategoty] = useState(categories[0]);
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>Sidebar</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills
-            categories={categories}
-            selectedCategory={selectedCategory}
-            onSelect={setSelectedCategoty}
-          />
+        <div className="overflow-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills
+              categories={categories}
+              selectedCategory={selectedCategory}
+              onSelect={setSelectedCategory}
+            />
+          </div>
         </div>
       </div>
     </div>
